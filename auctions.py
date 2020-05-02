e757d9936b5c60c78792b9bd490874c014b4e8e2
mathijs <mahelderman@gmail.com>
2020-05-02 16:34:22 +0200

    auction
    Auction class plus list of first_urls

---
+++ auctions.py
@@ -0,0 +1,44 @@
+import datetime
+
+
+class Auction:
+    """
+    Auction of watches
+    """
+
+    def __init__(self, first_url, date='', currency='USD', number_of_lots=0):
+        self.first_url = first_url
+        self.date = date
+        self.currency = currency
+        self.number_of_lots = number_of_lots
+
+
+auctions = [
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-f-p-journe-piaget-the-atmos-clock/f-p-journe-chronometre-a-resonance-an-early', '29-04-2020', 'HKD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-a-lange-soehne-and-swiss/a-lange-soehne-grand-lange-1-moon-phase-lumen', '22-04-2020', 'HKD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-patek-philippe/patek-philippe-nautilus-reference-5980-a-brand-new', '15-04-2020', 'HKD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-rolex-and-audemars-piguet/rolex-submariner-hulk-reference-116610lv-a', '08-04-2020', 'HKD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/f-p-journe-centigraphe-souverain-ref-cts-aluminium', '26-03-2020', 'CHF'),
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online/patek-philippe-ref-5712g-nautilus-a-white-gold', '11-03-2020', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/important-watches/rolex-ref-116710blnr-gmt-master-ii-batman-a', '11-12-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-8/rolex-gmt-master-reference-16700-stainless-steel', '05-12-2019', 'GBP'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-7/rolex-a-crystal-paperweight-made-for-baselworld', '26-11-2019', 'HKD'),
+    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-db1902/lot.1.html',
+            '24-11-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-ge1904/lot.201.html', '12-11-2019', 'CHF'),
+    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-hk0892/lot.2001.html', '08-10-2019', 'HKD'),
+    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-l19071/lot.1.html',
+            '24-09-2019', 'GBP'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-6/rolex-ref-116523-daytona-a-stainless-steel-and', '19-09-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-5/rolex-kermit-flat-4-submariner-ref-16610lv', '10-09-2019', 'CHF'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-the-drivers-collection/rolex-submariner-ref-116610ln-a-stainless-steel', '20-08-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/omega-speedmaster-to-the-moon-and-back/mebel-a-plastic-rectangular-vide-poche-depicting', '19-07-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-4/iwc-schaffhausen-pilots-watch-le-petit-prince', '17-07-2019', 'GBP'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-3/piaget-diamond-tradition-heart-a-white-gold-and', '13-06-2019', 'HKD'),
+    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-n10082/lot.1.html', '04-06-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/the-rolex-daytona-capsule-collection/rolex-daytona-ref-116500ln-a-stainless-steel', '30-05-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-2/0111495e-667c-4e7b-8e5f-4a7e28b97575', '02-04-2019', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online/16197530-ad43-47bb-87d6-43eac6b5f8ca', '25-02-2019', 'CHF'),
+    Auction('https://www.sothebys.com/en/buy/auction/2018/watches-online/7b2d857d-040f-4757-9739-88f21fc5280b', '11-12-2018', 'USD'),
+    Auction('https://www.sothebys.com/en/buy/auction/2018/vacheron-constantin-the-vintage-collection/0054512e-aba6-4f2a-a9a8-ccde05d0854a', '15-10-2018', 'GBP'),
+]
