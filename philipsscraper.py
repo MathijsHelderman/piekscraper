dbd4966f5294a0f1eef3cf067c57f2418903374e
juanalb <juan@zimmermanzimmerman.nl>
2020-05-03 10:22:05 +0200

    update philipsscrapy.py

--- philipsscraper.py
+++ philipsscraper.py
@@ -2,6 +2,9 @@
 # 21/04/2020, 21:30 - 23.00
 # 21/04/2020, 00:45 - 01.30
 # 22/04/2020, 20:30 - 22.30
+# 24/04/2020, 23:30 - 00.00
+# 25/04/2020, 20:30 - 23:00
+# next todo: clean up data, split estimate 
 
 # AUCTION ID's (30)
 # https://www.phillips.com/auctions/auction/NY080119
@@ -46,10 +49,14 @@
 # 4: Als we dan de eerste lot pakken, zien we de volgende link 3.
 # 5: Deze link delen we op in stukjes: {base-url}/detail/patek-philippe/{auction_id}/{lot_id}
 # 6: Als we dan de lot_id verhogen met 1, komt de volgende lot(horloge) in beeld.
-# 7: Omdat we weten hoeveel lots er per auction zijn kunnen we alle lots van een auction af door de lot_id te verhogen met 1.
+# 7: Omdat we weten hoeveel lots er per auction zijn kunnen we gemakellijk alle lots van een auction af door de lot_id te verhogen met 1.
 
 import requests
+import re
+from openpyxl import Workbook
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
 
 class Auction:
     "Auction of the watch department"
@@ -58,20 +65,23 @@ class Auction:
         self.url = url
         self.number_of_lots = number_of_lots
 
+@dataclass
 class Watch: 
     "A watch sold at the auction"
 
-    def __init__(self, manufacturer, year, reference_number, model_name, sold_for, estimate, date_sold, material, case_number, description_condition, diameter = None, movement_number = None, calibre = None, bracelet_strap = None, accessoires = None, signed = None ):
+    def __init__(self, manufacturer = None, year = None, reference_number = None, model_name = None, sold_for = None, estimate_minimum = None, estimate_maximum = None, date_sold = None, material = None, case_number = None, description_condition = None, diameter = None, movement_number = None, calibre = None, bracelet_strap = None, accessoires = None, signed = None ):
         # Mandatory
         self.manufacturer = manufacturer
         self.year = year
         self.reference_number = reference_number
         self.model_name = model_name
         self.sold_for = sold_for
-        self.estimate_minimum = estimate
+        self.estimate_minimum = estimate_minimum
+        self.estimate_maximum = estimate_maximum
         self.date_sold = date_sold
         self.material = material
         self.case_number = case_number
+        self.description_condition = description_condition
         # Optional
         self.diameter = diameter
         self.movement_number = movement_number
@@ -79,8 +89,10 @@ class Watch:
         self.bracelet_strap = bracelet_strap
         self.accessoires = accessoires
         self.signed = signed
-        
-# Dit zijn alle eerste pagina's van een auction, met het totaal aantal lots(watches) in een auction
+
+    def __repr__(self):  
+        return "manufacturer: %s \nyear: %s \nref_num: %s \nmodel_name: %s \nsold_for: %s \nestimate_min: %s \nestimate_maximum: %s \ndate_sold: %s \nmaterial: %s \ncase_num: %s \ndescription_condition: %s \ndiameter: %s \nmovement_number: %s \ncalibre: %s \nbracelet_strap: %s \naccesoires: %s \nsigned: %s" % (self.manufacturer, self.year, self.reference_number, self.model_name, self.sold_for, self.estimate_minimum, self.estimate_maximum, self.date_sold, self.material, self.case_number, self.description_condition, self.diameter, self.movement_number, self.calibre, self.bracelet_strap, self.accessoires, self.signed)      
+    
 auctions_list = []
 auctions_list.append(Auction("https://www.phillips.com/detail/rolex/NY080119/1" ,75))
 auctions_list.append(Auction("https://www.phillips.com/detail/rolex/HK080319/801" ,321)) 
@@ -117,48 +129,129 @@ auctions_list.append(Auction("https://www.phillips.com/detail/rolex/CH080115/1" 
 watches_list = [] 
 
 # for each auction in auction_list
-for i in range(1):
+for auction_id in range(len(auctions_list)):
+    print("auction_id: %s" %(auction_id))
+    # for each lot in auction
+    for lot_index in range(auctions_list[auction_id].number_of_lots):
+        # GET_PAGE()
+        # van 1 tot number_of_lots
+        res = auctions_list[auction_id].url.rsplit("/", 1)
+        lot_id = int(res[1]) + lot_index
+        url = res[0] + "/" + str(lot_id)
+        print(url)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-    url = auctions_list[0].url
-    page = requests.get(url)
+        try:
+            info_elements = soup.find("ul", class_="info-list").find("li").find("p").find_all("span")
+        except Exception as inst:
+            print("WARNING: No lot on lot_id: " + str(lot_id) + " and " + "auction_id: " + str(auction_id))
+            break
+        
+        # 1 Loop door alle "spans"
+        watch = Watch()
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+        # get_watch
+        for k in range(len(info_elements)):
+            # 2 Check welke "strong" hoort bij een watch attribuut
+            attribute = info_elements[k].find("strong").get_text()[:-1]
 
-    info_elements = soup.find("ul", class_="info-list").find("li").find("p").find_all("span")
-    # Dit werkt dus niet voor alle paginas, aangezien er soms velden niet zijn waardoor de index niet meer klopt
-    # DUS
-    # 1 Loop door alle "spans"
-    # 2 Check welke "strong" hoort bij een watch attribuut
-    # 3 Voeg de "text" toe aan watch attribuut
+            if attribute == "Manufacturer":
+                # 3 Voeg de "text" toe aan watch attribuut
+                manufacturer = info_elements[k].find("text").get_text()
+                watch.manufacturer = manufacturer
+                
+            if attribute == "Year":
+                year = info_elements[k].find("text").get_text()
+                watch.year = year
+                
+            if attribute == "Reference No":
+                reference_number = info_elements[k].find("text").get_text()
+                watch.reference_number = reference_number
+                
+            if attribute == "Movement No":
+                movement_number = info_elements[k].find("text").get_text()
+                watch.movement_number = movement_number
+                        
+            if attribute == "Case No": 
+                case_number = info_elements[k].find("text").get_text()
+                watch.case_number = case_number
+                
+            if attribute == "Model Name":
+                model_name = info_elements[k].find("text").get_text()
+                watch.model_name = model_name
+                
+            if attribute == "Material":
+                material = info_elements[k].find("text").get_text()
+                watch.material = material
+                
+            if attribute == "Calibre":      
+                calibre = info_elements[k].find("text").get_text()
+                watch.calibre = calibre
+                
+            if attribute == "Bracelet/Strap":
+                bracelet_strap = info_elements[k].find("text").get_text()
+                watch.bracelet_strap = bracelet_strap
+                
+            if attribute == "Dimensions":
+                diameter = info_elements[k].find("text").get_text()
+                if diameter.endswith('Diameter'):
+                    diameter = diameter[:-8]
+                else:
+                    print("WARNING: Irregular diameter on lot_id: " + str(lot_id) + " and " + "auction_id: " + str(auction_id))
+                    watch.diameter = diameter    
+            # if attribute == "Clasp/Buckle":
+            #     clasp_buckle = info_elements[i].find("text").get_text()
+            #     
+            if attribute == "Signed":
+                signed = info_elements[k].find("text").get_text()
+                watch.signed = signed
+                
+            if attribute == "Accessories":
+                accessoires = info_elements[k].find("text").get_text()
+                watch.accessoires = accessoires
+        try:
+            sold_for = soup.find("p", class_="lot-detail-header__sold").get_text()
+        except Exception as inst:
+            print("WARNING: No lot on lot_id: " + str(lot_id) + " and " + "auction_id: " + str(auction_id))
 
-    manufacturer = info_elements[0].find("text").get_text()
-    year = info_elements[1].find("text").get_text()
-    reference_number = info_elements[2].find("text").get_text()
-    movement_number = info_elements[3].find("text").get_text()
-    case_number = info_elements[4].find("text").get_text()
-    model_name = info_elements[5].find("text").get_text()
-    material = info_elements[6].find("text").get_text()
-    calibre = info_elements[7].find("text").get_text()
-    bracelet_strap = info_elements[8].find("text").get_text()
-    diameter = info_elements[10].find("text").get_text() #TODO: format without "Diameter"
-    signed = info_elements[11].find("text").get_text()
-    accessoires = info_elements[12].find("text").get_text()
+        if sold_for.startswith('sold for'):
+            watch.sold_for = sold_for[-8:]
+        else:
+            print("WARNING: Irregular sold_For on lot_id: " + str(lot_id) + " and " + "auction_id: " + str(auction_id))
+            watch.sold_for = sold_for
 
-    sold_for = soup.find("p", class_="lot-detail-header__sold").get_text() #TODO: format without "SOLD FOR"
-    estimate = soup.find("p", class_="lot-detail-header__estimate").find("br").get_text()
-    date_sold = soup.find("div", class_="sale-title-banner").find("a").find("span").get_text()
+        estimate_element = soup.find("p", class_="lot-detail-header__estimate")
+        if estimate_element is not None:
+            watch.estimate_minimum = "$" + estimate_element.contents[4]
+            watch.estimate_maximum = "$" + estimate_element.contents[8]
+        else: 
 
-    description_condition = soup.find("p", class_="lot-detail-header__title").find("em").get_text()
+            watch.estimate_minimum = "Estimate on Request" 
+            watch.estimate_maximum = "Estimate on Request" 
 
-    watch = Watch(manufacturer, year, reference_number, model_name, sold_for, estimate, date_sold, material, case_number, description_condition, diameter, movement_number, calibre, bracelet_strap, accessoires, signed)
-    watches_list.append(watch)
+        date_sold = soup.find("div", class_="sale-title-banner").find("a").find("span").get_text()
+        regExp_result = re.search(r"\d", date_sold)
+        if regExp_result is not None:
+            digit_position = regExp_result.start()
+            watch.date_sold = date_sold[digit_position:999]
+        else:
+            print("WARNING: Irregular date_sold on lot_id: " + str(lot_id) + " and " + "auction_id: " + str(auction_id))
+            watch.date_sold = date_sold
 
-    attributes_of_watch = vars(watch)
-    print(', '.join("%s: %s" % item for item in attributes_of_watch.items()))
+        description_condition = soup.find("p", class_="lot-detail-header__title").find("em").get_text()
+        watch.description_condition = description_condition
+        watches_list.append(watch)
+        print("Successfully scraped watch on lot_id: " + str(lot_id) + " and " + "auction_id: " + str(auction_id))
+
+    workbook = Workbook()
+    sheet = workbook.active
+
+    # Append column names first
+    sheet.append(["manufacturer", "year", "reference_number", "model_name", "sold_for", "estimate_minimum", "estimate_maximum", "date_sold", "material", "case_number", "description_condition", "diameter", "movement_number", "calibre", "bracelet_strap", "accessoires", "signed"])
+
+    for wa in watches_list:
+        sheet_data = [wa.manufacturer, wa.year, wa.reference_number, wa.model_name, wa.sold_for, wa.estimate_minimum, wa.estimate_maximum, wa.date_sold, wa.material, wa.case_number, wa.description_condition, wa.diameter, wa.movement_number, wa.calibre, wa.bracelet_strap, accessoires, wa.signed]
+        sheet.append(sheet_data)   
     
-#TODO: Go to next watch -> End of auction ? -> go to next auction -> ...
-#TODO: Create xlsx file
-#TODO: Create columns from watch attribute names
-#TODO: Append each watch as a record 
-#fin
-
+    workbook.save(filename="hello_world.xlsx")
