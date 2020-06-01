9cf3d2ea26c1d71a950f658fb2bf4f7236695818
mathijs <mahelderman@gmail.com>
2020-06-01 03:50:10 +0200

    Added the test url's / auctions to the auctions file

--- sothebys_auctions.py
+++ sothebys_auctions.py
@@ -1,43 +1,67 @@
 from auction import Auction
 
+# auctions = [
+#     Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-f-p-journe-piaget-the-atmos-clock/f-p-journe-chronometre-a-resonance-an-early', '29-04-2020', 'HKD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-a-lange-soehne-and-swiss/a-lange-soehne-grand-lange-1-moon-phase-lumen', '22-04-2020', 'HKD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-patek-philippe/patek-philippe-nautilus-reference-5980-a-brand-new', '15-04-2020', 'HKD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-rolex-and-audemars-piguet/rolex-submariner-hulk-reference-116610lv-a', '08-04-2020', 'HKD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/f-p-journe-centigraphe-souverain-ref-cts-aluminium', '26-03-2020', 'CHF'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online/patek-philippe-ref-5712g-nautilus-a-white-gold', '11-03-2020', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/important-watches/rolex-ref-116710blnr-gmt-master-ii-batman-a', '11-12-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-8/rolex-gmt-master-reference-16700-stainless-steel', '05-12-2019', 'GBP'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-7/rolex-a-crystal-paperweight-made-for-baselworld', '26-11-2019', 'HKD'),
+#     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-db1902/lot.1.html',
+#             '24-11-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-ge1904/lot.201.html', '12-11-2019', 'CHF'),
+#     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-hk0892/lot.2001.html', '08-10-2019', 'HKD'),
+#     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-l19071/lot.1.html',
+#             '24-09-2019', 'GBP'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-6/rolex-ref-116523-daytona-a-stainless-steel-and', '19-09-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-5/rolex-kermit-flat-4-submariner-ref-16610lv', '10-09-2019', 'CHF'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-the-drivers-collection/rolex-submariner-ref-116610ln-a-stainless-steel', '20-08-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/omega-speedmaster-to-the-moon-and-back/mebel-a-plastic-rectangular-vide-poche-depicting', '19-07-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-4/iwc-schaffhausen-pilots-watch-le-petit-prince', '17-07-2019', 'GBP'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-3/piaget-diamond-tradition-heart-a-white-gold-and', '13-06-2019', 'HKD'),
+#     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-n10082/lot.1.html', '04-06-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/the-rolex-daytona-capsule-collection/rolex-daytona-ref-116500ln-a-stainless-steel', '30-05-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-2/0111495e-667c-4e7b-8e5f-4a7e28b97575', '02-04-2019', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online/16197530-ad43-47bb-87d6-43eac6b5f8ca', '25-02-2019', 'CHF'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2018/watches-online/7b2d857d-040f-4757-9739-88f21fc5280b', '11-12-2018', 'USD'),
+#     Auction('https://www.sothebys.com/en/buy/auction/2018/vacheron-constantin-the-vintage-collection/0054512e-aba6-4f2a-a9a8-ccde05d0854a', '15-10-2018', 'GBP'),
+# ]
+
+#
+# URL's of specific watches (for testing)
+#
 auctions = [
-    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-f-p-journe-piaget-the-atmos-clock/f-p-journe-chronometre-a-resonance-an-early', '29-04-2020', 'HKD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-a-lange-soehne-and-swiss/a-lange-soehne-grand-lange-1-moon-phase-lumen', '22-04-2020', 'HKD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-patek-philippe/patek-philippe-nautilus-reference-5980-a-brand-new', '15-04-2020', 'HKD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-weekly-rolex-and-audemars-piguet/rolex-submariner-hulk-reference-116610lv-a', '08-04-2020', 'HKD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/f-p-journe-centigraphe-souverain-ref-cts-aluminium', '26-03-2020', 'CHF'),
-    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online/patek-philippe-ref-5712g-nautilus-a-white-gold', '11-03-2020', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/important-watches/rolex-ref-116710blnr-gmt-master-ii-batman-a', '11-12-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-8/rolex-gmt-master-reference-16700-stainless-steel', '05-12-2019', 'GBP'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-7/rolex-a-crystal-paperweight-made-for-baselworld', '26-11-2019', 'HKD'),
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-db1902/lot.1.html',
-            '24-11-2019', 'USD'),
+            '01-01-1000', 'EUR'),  # test
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-db1902/lot.10.html#jumpToLotPopover',
-            '24-11-2019', 'USD'),  # test
+            '01-01-1000', 'EUR'),  # test
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2007/important-watches-hk0247/lot.1201.html',
-            '24-11-2019', 'USD'),  # test
+            '01-01-1000', 'EUR'),  # test
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2007/important-watches-ge0702/lot.34.html',
-            '24-11-2019', 'USD'),  # test
+            '01-01-10009', 'EUR'),  # test
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2009/important-watches-ge0901/lot.1.html',
-            '10-05-2009', 'CHF'),  # TEST
+            '01-01-1000', 'EUR'),  # TEST
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2009/important-watches-ge0901/lot.158.html',
-            '10-05-2009', 'CHF'),  # TEST
+            '01-01-1000', 'EUR'),  # TEST
     Auction('https://www.sothebys.com/en/auctions/ecatalogue/2018/watches-online-l18056/lot.1.html',
-            '10-05-2009', 'CHF'),  # TEST
-    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-ge1904/lot.201.html', '12-11-2019', 'CHF'),
-    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-hk0892/lot.2001.html', '08-10-2019', 'HKD'),
-    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/watches-l19071/lot.1.html',
-            '24-09-2019', 'GBP'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-6/rolex-ref-116523-daytona-a-stainless-steel-and', '19-09-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-5/rolex-kermit-flat-4-submariner-ref-16610lv', '10-09-2019', 'CHF'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-the-drivers-collection/rolex-submariner-ref-116610ln-a-stainless-steel', '20-08-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/omega-speedmaster-to-the-moon-and-back/mebel-a-plastic-rectangular-vide-poche-depicting', '19-07-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-4/iwc-schaffhausen-pilots-watch-le-petit-prince', '17-07-2019', 'GBP'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-3/piaget-diamond-tradition-heart-a-white-gold-and', '13-06-2019', 'HKD'),
-    Auction('https://www.sothebys.com/en/auctions/ecatalogue/2019/important-watches-n10082/lot.1.html', '04-06-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/the-rolex-daytona-capsule-collection/rolex-daytona-ref-116500ln-a-stainless-steel', '30-05-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online-2/0111495e-667c-4e7b-8e5f-4a7e28b97575', '02-04-2019', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2019/watches-online/16197530-ad43-47bb-87d6-43eac6b5f8ca', '25-02-2019', 'CHF'),
-    Auction('https://www.sothebys.com/en/buy/auction/2018/watches-online/7b2d857d-040f-4757-9739-88f21fc5280b', '11-12-2018', 'USD'),
-    Auction('https://www.sothebys.com/en/buy/auction/2018/vacheron-constantin-the-vintage-collection/0054512e-aba6-4f2a-a9a8-ccde05d0854a', '15-10-2018', 'GBP'),
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/longines-yellow-gold-wristwatch-circa-1940',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/longines-double-hand-ref-5699-stainless-steel',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/a-lange-soehne-lange-1-mondphase-ref-109-032-pink',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/rolex-panama-canal-submariner-ref-16613-limited',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/rolex-double-red-sea-dweller-ref-1665-stainless',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/de-beers-gold-plated-hourglass-with-floating',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/patek-philippe-nautilus-ref-5711-stainless-steel',
+            '01-01-1000', 'EUR'),  # TEST
+    Auction('https://www.sothebys.com/en/buy/auction/2020/watches-online-2/f-p-journe-centigraphe-souverain-ref-cts-aluminium',
+            '01-01-1000', 'EUR'),  # TEST
 ]
